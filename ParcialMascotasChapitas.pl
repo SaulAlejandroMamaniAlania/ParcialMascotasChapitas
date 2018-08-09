fb0cbcdb55b4eca65a51a14c675c982bf0c6d76a
SaulAlejandroMamaniAlania <smamanialania@est.frba.utn.edu.ar>
2018-08-09 18:50:33 -0300

    base de conocimiento

---
+++ ParcialMascotasChapitas.pl
@@ -0,0 +1,18 @@
+adopto(martin,pepa,2014).
+adopto(martin,frida,2015).
+
+
+
+
+mascota(pepa, perro(mediano)).
+mascota(frida, perro(grande)).
+mascota(piru, gato(macho,15)).
+mascota(kali, gato(macho,3)).
+mascota(olivia, gato(hembra,16)).
+mascota(mambo, gato(macho,2)).
+mascota(abril, gato(hembra,4)).
+mascota(buenaventura, tortuga(agresiva)).
+mascota(severino, tortuga(agresiva)).
+mascota(simon, tortuga(tranquila)).
+mascota(quinchin, gato(macho,0)).
+
